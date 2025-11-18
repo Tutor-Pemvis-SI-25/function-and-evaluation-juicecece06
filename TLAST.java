6ee0fb1d03e51fd02ad8a31b4ea64f8bdfefbd52
juicecece06 <juniatijusmerjuniati@gmail.com>
2025-11-18 21:20:33 +0700

    JUSMER

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,27 @@
-//Nama - NIM
+//JUSMER-12S25012
 
+import java.util.*;
+import java.lang.Math;
+
+public class TLAST {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] taskList = new int[10];
+        int taskCount;
+        String command;
+
+        taskCount = 0;
+        command = input.nextLine();
+        while (!command.equals("---") && taskCount < 10) {
+        }
+    }
+    
+    public static void addTask(int[] tugas, int count) {
+    }
+    
+    public static double calculatePriority(int difficulty, int days) {
+        
+        return 10;
+    }
+}
